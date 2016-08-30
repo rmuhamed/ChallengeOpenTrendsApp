0a83dddb09844c2c9c0a4b3cdee9241dfa562691
rmuhamed <ricardo.muhamed@gmail.com>
2016-08-30 20:21:43 -0300

    Initial commit

---
+++ app/src/main/java/com/rmuhamed/demoapp/activity/ItemListActivity.java
@@ -0,0 +1,18 @@
+package com.rmuhamed.demoapp.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.rmuhamed.demoapp.R;
+
+public class ItemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_item_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.setSupportActionBar(toolbar);
+    }
+}
---
+++ app/src/main/java/com/rmuhamed/demoapp/activity/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.rmuhamed.demoapp.activity;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rmuhamed.demoapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_splash);
+
+        this.runSplash();
+    }
+
+    private void runSplash() {
+        new CountDownTimer(2000, 1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                launchNextActivity();
+            }
+        };
+    }
+
+    private void launchNextActivity() {
+        Intent nextActivityIntent = new Intent(this, ItemListActivity.class);
+
+        this.startActivity(nextActivityIntent);
+        this.finish();
+    }
+}
