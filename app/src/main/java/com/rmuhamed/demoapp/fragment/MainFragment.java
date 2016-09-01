e0e40d2bc78369db5197b561bcec303022d2acc6
rmuhamed <ricardo.muhamed@gmail.com>
2016-09-01 06:50:36 -0300

    Kickoff - Layout render (Look & Feel aspects)

--- app/src/main/java/com/rmuhamed/demoapp/fragment/MainFragment.java
+++ app/src/main/java/com/rmuhamed/demoapp/fragment/MainFragment.java
@@ -3,6 +3,7 @@ package com.rmuhamed.demoapp.fragment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +21,10 @@ public class MainFragment extends Fragment implements GetEntitiesRequestCallback
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View contentView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        return contentView;
     }
 
     @Override
