3e11719ff224dafa8577e301e827b70edc81789d
rmuhamed <ricardo.muhamed@gmail.com>
2016-09-01 07:02:30 -0300

    Butterknife - Integrations & First steps with Main Fragment

--- app/src/main/java/com/rmuhamed/demoapp/fragment/MainFragment.java
+++ app/src/main/java/com/rmuhamed/demoapp/fragment/MainFragment.java
@@ -3,6 +3,7 @@ package com.rmuhamed.demoapp.fragment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +16,13 @@ import com.rmuhamed.demoapp.model.Entity;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainFragment extends Fragment implements GetEntitiesRequestCallback {
+
+    @BindView(R.id.recycler_list)
+    RecyclerView anEntityRecyclerList;
 
     public MainFragment() {}
 
@@ -23,6 +30,9 @@ public class MainFragment extends Fragment implements GetEntitiesRequestCallback
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View contentView = inflater.inflate(R.layout.fragment_main, container, false);
 
+        ButterKnife.bind(this, contentView);
+
+        this.setupRecyclerList();
 
         return contentView;
     }
@@ -43,5 +53,13 @@ public class MainFragment extends Fragment implements GetEntitiesRequestCallback
     @Override
     public void onError(String errorMessage) {
         //TODO: Inform error using SnackBar with retry logic
+    }
+
+    private void setupRecyclerList() {
+        LinearLayoutManager aLayoutManager = new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false);
+
+        this.anEntityRecyclerList.setLayoutManager(aLayoutManager);
+        this.anEntityRecyclerList.setAdapter(null);
+
     }
 }
