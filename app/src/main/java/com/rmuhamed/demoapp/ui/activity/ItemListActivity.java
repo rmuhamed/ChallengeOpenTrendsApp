3f960300d78f2fabf6079f982a4d218ac3a2f227
rmuhamed <ricardo.muhamed@gmail.com>
2016-09-03 09:04:57 -0300

    Performing Fragment Logic

--- app/src/main/java/com/rmuhamed/demoapp/ui/activity/ItemListActivity.java
+++ app/src/main/java/com/rmuhamed/demoapp/ui/activity/ItemListActivity.java
@@ -1,10 +1,14 @@
 package com.rmuhamed.demoapp.ui.activity;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.rmuhamed.demoapp.R;
+import com.rmuhamed.demoapp.ui.fragment.MainFragment;
 
 public class ItemListActivity extends AppCompatActivity {
 
@@ -12,7 +16,21 @@ public class ItemListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_item_list);
+
+        this.setupToolbar();
+
+        this.launchFragment(new MainFragment());
+    }
+
+    private void setupToolbar() {
         Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
         this.setSupportActionBar(toolbar);
+    }
+
+    private void launchFragment(Fragment aFragment) {
+        FragmentTransaction fragmentTransaction = this.getSupportFragmentManager().beginTransaction();
+
+        fragmentTransaction.add(R.id.root_container, aFragment);
+        fragmentTransaction.commit();
     }
 }
