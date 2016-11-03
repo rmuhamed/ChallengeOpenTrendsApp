01753f49febafb91be8fd8c66048862c464606a1
rmuhamed <ricardo.muhamed@gmail.com>
2016-11-03 15:30:55 +0100

    Adjustments to handle new features by circleci

--- app/src/main/java/com/rmuhamed/demoapp/ui/activity/SplashActivity.java
+++ app/src/main/java/com/rmuhamed/demoapp/ui/activity/SplashActivity.java
@@ -5,10 +5,13 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
 import com.rmuhamed.demoapp.R;
 import com.rmuhamed.demoapp.utils.ImageLoader;
+
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,9 +19,15 @@ import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends AppCompatActivity {
     public static final int SPLASH_DURATION = 3000;
+    public static final int TICK_INTERVAL = 1000;
+
+    private int remainingTime = 5;
 
     @BindView(R.id.splash_picture)
     ImageView splashPicture;
+
+    @BindView(R.id.remaining_time_label)
+    TextView remainingTimeLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +48,11 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void performSplash(int splashDuration) {
-        new CountDownTimer(splashDuration, 1000) {
+        new CountDownTimer(splashDuration, TICK_INTERVAL) {
             @Override
-            public void onTick(long l) {}
+            public void onTick(long l) {
+                SplashActivity.this.remainingTimeLabel.setText(String.format(Locale.getDefault(), "%d sec", --remainingTime));
+            }
 
             @Override
             public void onFinish() { SplashActivity.this.launchNextActivity();}
